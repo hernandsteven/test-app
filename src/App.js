6b24a411d5154ce9167003857c364e2e74d41e4b
Steven Hernandez <hernandsteven@gmail.com>
2022-06-09 01:00:50 -0400

    fixed dropdown / added axios, async data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Counter } from './features/counter/Counter';
-import Home from './pages/Home';
-import Prime from './pages/Prime';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Counter } from "./features/counter/Counter";
+import Home from "./pages/Home";
+import Prime from "./pages/Prime";
+import Vanilla from "./pages/Vanilla";
 
 export const pathName = {
-  HOME: '/',
-  COUNTER: '/Counter',
-  PRIME: '/Prime',
+  HOME: "/",
+  COUNTER: "/Counter",
+  PRIME: "/Prime",
+  VANILLA: "/Vanilla",
 };
 
 const useRoutes = [
   { path: pathName.HOME, component: <Home />, enabled: true },
   { path: pathName.COUNTER, component: <Counter />, enabled: true },
   { path: pathName.PRIME, component: <Prime />, enabled: true },
+  { path: pathName.VANILLA, component: <Vanilla />, enabled: true },
 ];
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,5 +13,5 @@ ReactDOM.render(
     </Provider>
   </BrowserRouter>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
